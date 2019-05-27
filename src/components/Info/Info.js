a87af02c74a69147d8d7723b48971b3cc7087cef
Alexander Makarof <makarofalex@gmail.com>
2019-05-27 08:39:43 +0300

    added icons

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -1,23 +1,19 @@
 import React from 'react';
-import icon_calendar from '../../resources/images/icons/calendar.png';
-import icon_location from '../../resources/images/icons/location.png';
 import Zoom from 'react-reveal/Zoom';
 
 const Info = () => {
   return (
-    <div className='bck_black'>
-      <div className='center_wrapper'>
-        <div className="vn_wrapper">
-          <Zoom duration={1500}>
-            <div> App Store </div>
-          </Zoom>
-          <Zoom duration={2500}>
-            <div>Google play</div>
-          </Zoom>
-          <Zoom duration={3500}>
-            <div>Gmail</div>
-          </Zoom>
-        </div>
+    <div className='center_wrapper'>
+      <div className="vn_wrapper">
+        <Zoom duration={1500}>
+          <a><i className="fab fa-app-store" style={{ fontSize: '48px' }} /></a>
+        </Zoom>
+        <Zoom duration={2500}>
+          <a><i className="fab fa-google-play" style={{ fontSize: '48px' }} /></a>
+        </Zoom>
+        <Zoom duration={3500}>
+          <div></div>
+        </Zoom>
       </div>
     </div>
   );
--- src/components/features/FeaturedIndex.js
+++ src/components/features/FeaturedIndex.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import Carrousel from './Carrousel';
 
-import metLogo from './../../resources/images/metLogo.svg';
+
 
 const FeaturedIndex = () => {
     return (
         <div style={{ positioin: 'relative', }}>
             <Carrousel />
             <div className='img-wrapper'>
-                {/* <img className='img-logo'
-                    src={metLogo}
-                /> */}
+
                 <p className='center' style={{
                     textAlign: 'center',
                     fontSize: '40px',
                     fontWeight: 'bold',
-                    color: 'yellow'
+                    color: 'red'
                 }}>Пацан сказал, пацан сделал</p>
             </div>
 
